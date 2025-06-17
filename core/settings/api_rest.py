14823519c82f2a622c078def480b4384d91f051d
tioRaffa <rafaelmuniz200@gmail.com>
2025-06-17 09:41:09 -0300

    Adiciona estrutura inicial do projeto Django com configurações básicas, arquivos de ambiente e utilitários

---
+++ core/settings/api_rest.py
@@ -0,0 +1,19 @@
+from datetime import timedelta
+import os
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
+}
+
+# SIMPLE_JWT = {
+#     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+#     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+#     "ROTATE_REFRESH_TOKENS": False,
+#     "BLACKLIST_AFTER_ROTATION": False,
+    
+#     "SIGNING_KEY": os.environ.get('SECRET_KEY_JWT', 'INSECURE'),
+
+#     "AUTH_HEADER_TYPES": ("Bearer",),
+# }
---
+++ core/settings/installed_apps.py
@@ -0,0 +1,11 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Django restframework / JWT
+    'rest_framework',
+    # 'rest_framework_simplejwt',
+]
