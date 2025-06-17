caf1373a8e93fb343d99983fc481bee7eff5ca23
tioRaffa <rafaelmuniz200@gmail.com>
2025-06-17 12:07:47 -0300

    Remove configurações de paginação do Django REST Framework e ajusta a estrutura do Djoser para melhor organização das configurações de email

--- core/settings/djoser.py
+++ core/settings/djoser.py
@@ -1,17 +1,17 @@
 from decouple import config
 from .environment import DEBUG
 DJOSER = {
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/{uid}/{token}'
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/{uid}/{token}',
+    'PASSWORD_CONFIRM_RETYPE': True
 }
 
 
-if DEBUG:
-    EMAIL_HOST = config('EMAIL_HOST')
-    EMAIL_PORT = config('EMAIL_PORT')
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='webmaster@localhost')
 
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', 'webmaster@localhost')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
